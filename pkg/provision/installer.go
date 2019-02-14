3eb14265f6d0158a27cdb131d068a73df72aec7c
Erno Aapa <erno.aapa@gmail.com>
2019-02-14 09:58:19 +0200

    Skip action prompt by taking default action if conf file exist
    Make possible to have `/etc/default/kubelet` beforehand.

--- pkg/provision/installer.go
+++ pkg/provision/installer.go
@@ -64,7 +64,7 @@ func (i *Installer) installKube() error {
 		"curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -",
 		"sudo sh -c 'echo \"deb https://apt.kubernetes.io/ kubernetes-xenial main\" > /etc/apt/sources.list.d/kubernetes.list'",
 		"sudo apt-get update",
-		"sudo apt-get install -y kubelet kubeadm kubectl",
+		"sudo apt-get install -y -o Dpkg::Options::=--force-confdef kubelet kubeadm kubectl",
 		"sudo apt-mark hold kubelet kubeadm kubectl",
 	})
 }
